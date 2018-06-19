edc8265f437f3aa8e4d264c05062d48b3359757b
I809764 <thomas.jung@sap.com>
2018-06-19 14:25:36 -0400

    Initial 2018 Version

--- js/router/index.js
+++ js/router/index.js
@@ -1,14 +1,6 @@
+/*eslint-env node, es6 */
 "use strict";
 
-module.exports = function(app, server) {
-	app.use("/node", require("./routes/node")());
-	app.use("/node/ex1", require("./routes/ex1")());
-	app.use("/node/ex2", require("./routes/ex2")());
-	app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());
-	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
-	app.use("/node/textBundle", require("./routes/textBundle")());
-	app.use("/node/excel", require("./routes/excel")());
-	app.use("/node/xml", require("./routes/xml")());
-	app.use("/node/zip", require("./routes/zip")());
-	app.use("/node/chat", require("./routes/chatServer")(server));
+module.exports = (app, server) => {
+		 app.use("/node", require("./routes/node")());
 };
