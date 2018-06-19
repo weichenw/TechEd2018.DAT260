20d7c02d5a6dacd4bf93286150911becc04a8984
I809764 <thomas.jung@sap.com>
2018-06-19 17:38:02 -0400

    Ex1 and Ex2 - Simple DB parts

--- js/router/index.js
+++ js/router/index.js
@@ -2,5 +2,7 @@
 "use strict";
 
 module.exports = (app, server) => {
-		 app.use("/node", require("./routes/node")());
+	app.use("/node", require("./routes/node")());
+	app.use("/node/ex1", require("./routes/ex1")());
+	app.use("/node/ex2", require("./routes/ex2")());	
 };
