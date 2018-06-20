9c652bd6c3a5aa936a41022ef0879e138423d19d
jungsap <thomas.jung@sap.com>
2018-06-20 11:55:47 -0400

    Exercise 3.3 Update 2018

--- js/router/index.js
+++ js/router/index.js
@@ -5,4 +5,5 @@ module.exports = (app, server) => {
 	app.use("/node", require("./routes/node")());
 	app.use("/node/ex1", require("./routes/ex1")());
 	app.use("/node/ex2", require("./routes/ex2")());	
+	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));	
 };
