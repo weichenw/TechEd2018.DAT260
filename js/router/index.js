37f5d6b637ec067e20cc541682ba6c49db4c10c9
jungsap <thomas.jung@sap.com>
2018-06-20 15:38:10 -0400

    Ex 3.5 update for 2018

--- js/router/index.js
+++ js/router/index.js
@@ -4,7 +4,8 @@
 module.exports = (app, server) => {
 	app.use("/node", require("./routes/node")());
 	app.use("/node/ex1", require("./routes/ex1")());
-	app.use("/node/ex2", require("./routes/ex2")());	
-	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));	
-	app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());		
+	app.use("/node/ex2", require("./routes/ex2")());
+	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
+	app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());
+	app.use("/node/textBundle", require("./routes/textBundle")());
 };
