21a61ed6da319b15b9dde436cca5c265b3c223ec
jungsap <thomas.jung@sap.com>
2018-06-20 15:11:10 -0400

    Ex 3.4 JavaScriptBasics 2018 update

--- js/router/index.js
+++ js/router/index.js
@@ -6,4 +6,5 @@ module.exports = (app, server) => {
 	app.use("/node/ex1", require("./routes/ex1")());
 	app.use("/node/ex2", require("./routes/ex2")());	
 	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));	
+	app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());		
 };
